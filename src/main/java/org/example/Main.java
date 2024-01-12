a9b45d95f372b93808f6149d100227dfce4404a9
Caroline O'Brien-Barden <133952159+Carolineobb@users.noreply.github.com>
2024-01-12 23:07:31 +0100

    la till en huvudmeny

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Skapa instanser av varje konverteringsklass
+        ConvertArea areaConverter = new ConvertArea();
+        ConvertBinary binaryConverter = new ConvertBinary();
+        ConvertEnergy energyConverter = new ConvertEnergy();
+        ConvertLength lengthConverter = new ConvertLength();
+        ConvertSpeed speedConverter = new ConvertSpeed();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Låt användaren välja vilken konvertering de vill göra
+        String[] conversionOptions = {
+                "Area Conversion",
+                "Binary Conversion",
+                "Energy Conversion",
+                "Length Conversion",
+                "Speed Conversion"
+        };
+        String userChoice = (String) JOptionPane.showInputDialog(
+                null,
+                "Välj typ av konvertering:",
+                "Huvudmeny",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                conversionOptions,
+                conversionOptions[0]
+        );
+
+        // Anropa motsvarande metod i den valda konverteringsklassen
+        if (userChoice != null) {
+            switch (userChoice) {
+                case "Area Conversion":
+                    //areaConverter.runConversion();
+                    break;
+                case "Binary Conversion":
+                    //binaryConverter.runConversion();
+                    break;
+                case "Energy Conversion":
+                    //energyConverter.runConversion();
+                    break;
+                case "Length Conversion":
+                    lengthConverter.runConversion();
+                    break;
+                case "Speed Conversion":
+                    //speedConverter.runConversion();
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Ogiltigt val");
+            }
         }
     }
 }
